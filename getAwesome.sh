5a86497805486a5752c7ca6c408cebffea700967
jtcdbob <bob@houzz.com>
2017-03-14 14:42:04 -0700

    Init commit for my awesome dotfiles

---
+++ getAwesome.sh
@@ -0,0 +1,19 @@
+#### Variables
+
+home=~
+dotfilePath=.
+# subdirs="zsh bash git tmux vim"
+
+#### Create symlinks
+
+# for dir in `ls $dotfilePath | grep -v "*.sh"`;
+ls $dotfilePath | grep -v "\.sh" | while read dir
+do
+        for file in `ls $dir`;
+        do
+                target=$dotfilePath/$dir/$file
+                dest=$home/.$file
+                ln -s $target $dest
+                echo "Create symlink for $file"
+        done
+done
